7ae828e8048c70e8d2fca89bad340fc2d03afb9f
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-20 03:44:20 +0700

    constants

---
+++ hosting/src/constants/pages.ts
@@ -0,0 +1,72 @@
+import { Page } from '@/types'
+
+export const miscellaneousPages: Array<Page> = [
+  {
+    name: 'Side',
+    path: '/side',
+    components: []
+  },
+  {
+    name: 'Lab',
+    path: '/lab',
+    components: []
+  },
+  {
+    name: 'Past Exam',
+    path: '/pastexam',
+    components: []
+  },
+  {
+    name: 'Sources',
+    path: '/sources',
+    components: []
+  },
+  {
+    name: 'About',
+    path: '/about',
+    components: []
+  }
+]
+
+export const secondSemesterPages: Array<Page> = [
+  {
+    name: 'Chapter 1',
+    path: '/chapter-1',
+    components: []
+  },
+  {
+    name: 'Chapter 2',
+    path: '/chapter-2',
+    components: []
+  },
+  {
+    name: 'Chapter 3',
+    path: '/chapter-3',
+    components: []
+  },
+  {
+    name: 'Chapter 4-5',
+    path: '/chapter-4-5',
+    components: []
+  },
+  {
+    name: 'Chapter 6',
+    path: '/chapter-6',
+    components: []
+  },
+  {
+    name: 'Chapter 7',
+    path: '/chapter-7',
+    components: []
+  },
+  {
+    name: 'Chapter 8',
+    path: '/chapter-8',
+    components: []
+  },
+  {
+    name: 'Chapter 9-10',
+    path: '/chapter-9-10',
+    components: []
+  }
+]
