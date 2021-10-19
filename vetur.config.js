fcef0ff90cf7b3748e302fc3f87cc9dfc908c610
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-20 03:21:06 +0700

    vetur config

---
+++ vetur.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projects: [
+    './hosting',
+    {
+      root: './hosting',
+      package: './hosting/package.json',
+      tsconfig: './hosting/tsconfig.json'
+    }
+  ]
+}
