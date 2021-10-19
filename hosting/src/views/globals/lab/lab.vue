0d29e1f91d3d903bd195a8397ed9f7ffee01c284
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-20 04:06:20 +0700

    lab page

---
+++ hosting/src/views/globals/lab/lab.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="sources-page">
+  <section class="hero is-sailor-pink is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1 has-text-gray-border">
+          Laboratory
+        </h1>
+        <h3 class="subtitle is-3">
+          Lab week 1 - Lab week 12 To help you to understand for this subject
+        </h3>
+      </div>
+    </div>
+  </section>
+  <section class="hero is-white">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-2">
+          Laboratory
+        </h1>
+        <div class="content">
+          <ul>
+            <li>
+              <a href="https://drive.google.com/file/d/1CdbjxKXKEKIofsAUiryDTdNadJPHJ9e4/view?usp=sharing">Lab 1</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/17adqnqtlXQ4fR1NSWrYXxZPiNOAYeDau/view?usp=sharing">Lab 2</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1nTijBikzwiLeEP69VFYCll2H0pV_UEmL/view?usp=sharing">Lab 3</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1b8kphr6eNuIc57HV04NNNLPF4RkwSKwW/view?usp=sharing">Lab 4</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1A3e5YaIkroo13ESCOrhrjmirbIlc0oia/view?usp=sharing">Lab 5</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1FDhpQ3Fye2Nl8ikB4Lod8BzpdJf9eaR2/view?usp=sharing">Lab 6</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1t3WqwIUy74dGCmjs8ymQs-opynn5Hmgn/view?usp=sharing">Lab 7</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1ta37MIgOnXHfz4C7C9hD-CrAJrabaHOH/view?usp=sharing">Lab 8</a>
+            </li>
+            <li>
+                 <a href="https://drive.google.com/file/d/1RaOIvu-WklA_HoPbA4_xSUSQhNNt3i9v/view?usp=sharing">Lab 9</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1ic3XeCnHOAW-Dh5w0RJA_6PQcziSy4fx/view?usp=sharing">Lab 10</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1EwuyyXVJOzosPrsTHjP_V5CGPNdvUJMX/view?usp=sharing">Lab 11</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1SLnGGAH2rASgfpr4xyzkWk7bSv-mb9yb/view?usp=sharing">Lab 12</a>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-2">
+          Decimal Points
+        </h1>
+        <div class="content">
+          <ul>
+            <li>
+              https://www.splashlearn.com/math-vocabulary/decimals/decimal-point
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section-->
+  <!--section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-2">
+          Error icons
+        </h1>
+        <div class="content">
+          <ul>
+            <li>
+              <div>Icons made by <a href="https://www.flaticon.com/authors/nikita-golubev" title="Nikita Golubev">Nikita Golubev</a>
+                from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section-->
+  <breadcrumb-footer />
+</div>
+</template>
+<script lang="ts" src="./lab.ts"></script>
