b1fee2b2fb10627d812fc4519892457c9f6eb496
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-20 04:06:50 +0700

    side page

---
+++ hosting/src/views/globals/side/side.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="sources-page">
+  <section class="hero is-sailor-pink is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1 has-text-gray-border">
+          Side Materials
+        </h1>
+      </div>
+    </div>
+  </section>
+  <section class="hero is-white">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-2">
+          Year 2019-Now
+        </h1>
+        <div class="content">
+          <ul>
+            <li>
+              <a href="https://drive.google.com/file/d/1YCa6PGmPD823mdaNDYpcKVx2jT4LE49K/view">CH1(Intro)oop</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/12U8aiK86PWTkBUS2DzlwYhQeKlvDNGQL/view">CH2(dataType)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1th4EQ1JqkDZged_wkGIBBGn973OYp3c9/view">CH3(statement)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1IsOM9LvETpwoGr-hk2bBf6Ran66jJgMq/view">CH4(ClassAndObject)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1FaGZSa2AGSrHi3iCOsFvoFxP9a3MrRvR/view">CH5(ClassAndObjectII)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1wmvyEGP4PQrf6isdwHIlycgqOJFtRyuY/view">CH6(Inheritance)</a>
+            </li>
+             <li>
+              <a href="https://drive.google.com/file/d/1owQDsDZD9NyNg4_hkcaBcADXzm_TCuMW/view">CH7(AbstractClass)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1ltegbiEBryCK5765ngr7wrkW2jz2OtFP/view">CH8(CaseStudy)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1HxL4XUi6XYsGsnvitdKe-FVOI4qaAjWe/view">CH9(JFrame_Event)</a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1c_NpIwWxnUtu4prbrsk-EZCc4FmYp7xu/view">CH10(MoreEvent_LayOut)</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <breadcrumb-footer/>
+</div>
+</template>
+<script lang="ts" src="./side.ts"></script>
