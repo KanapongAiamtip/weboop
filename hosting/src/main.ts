94a379061b5b4d503f9148b8460526a4733eb133
ppromxx <kannaphongi62@nu.ac.th>
2022-04-05 03:41:33 +0700

    remove setting

--- hosting/src/main.ts
+++ hosting/src/main.ts
@@ -1,6 +1,5 @@
 import Buefy from 'buefy'
 import Vue from 'vue'
-import VueKatex from 'vue-katex'
 import VueMeta from 'vue-meta'
 
 import App from './App.vue'
@@ -8,12 +7,10 @@ import router from './router'
 
 import '@mdi/font/css/materialdesignicons.css'
 import './globals.scss'
-import 'katex/dist/katex.min.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
-Vue.use(VueKatex)
 Vue.use(VueMeta)
 
 new Vue({
